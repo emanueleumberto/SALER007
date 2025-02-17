db63ad772f311db416fd58fcc1d27163fb8ed22f
emanueleumberto <info@emanueleumberto.it>
2025-02-17 17:42:38 +0100

    Soluzione Esercizio

--- OOP2Java/src/Esercizio/Immagine.java
+++ OOP2Java/src/Esercizio/Immagine.java
@@ -1,4 +1,44 @@
 package Esercizio;
 
-public class Immagine {
+public class Immagine extends ElementoMultimediale implements GestioneLuminosita {
+
+    private int luminosita;
+
+    public Immagine(String titolo, int luminosita) {
+        super(titolo);
+        this.luminosita = luminosita;
+    }
+
+    public int getLuminosita() {
+        return luminosita;
+    }
+
+    public void show() {
+        System.out.println(this.getTitolo() + "*".repeat(this.luminosita));
+    }
+
+    @Override
+    public void aumentaLuminosita() {
+        if(this.luminosita < 10) {
+            this.luminosita++;
+            System.out.println("Luminosità regolata " + this.luminosita);
+        } else {
+            System.out.println("Luminosità massima raggiunta");
+        }
+    }
+
+    @Override
+    public void diminuisciLuminosita() {
+        if(this.luminosita > 0) {
+            this.luminosita--;
+            System.out.println("Luminosità regolata " + this.luminosita);
+        } else {
+            System.out.println("Luminosità minima raggiunta");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " Luminosità: " + this.luminosita;
+    }
 }
--- OOP2Java/src/Esercizio/RiproduttoreMultimediale.java
+++ OOP2Java/src/Esercizio/RiproduttoreMultimediale.java
@@ -1,11 +1,55 @@
 package Esercizio;
 
+import java.util.Scanner;
+
 public class RiproduttoreMultimediale {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         Audio a = new Audio("MiaCanzone", 4, 6);
         //a.play();
 
         Video v = new Video("MioVideo", 8, 6, 4);
-        v.play();
+        //v.play();
+
+        Immagine im = new Immagine("MiaImmagine", 8);
+        //i.show();
+
+        ElementoMultimediale[] multimedia = new ElementoMultimediale[5];
+        multimedia[0] = a;
+        multimedia[1] = v;
+        multimedia[2] = im;
+        multimedia[3] = null;
+        multimedia[4] = null;
+
+        do {
+            for (int i=0; i< multimedia.length; i++) {
+                if(multimedia[i] != null) {
+                    System.out.println(i + 1 + " - " + multimedia[i].getTitolo());
+                } else {
+                    System.out.println(i + 1 + " - Empty" );
+                }
+            }
+            System.out.print("Quale Elemento multimediale vuoi riprodurre? oppure 0 per finire");
+            int ele = Integer.parseInt(sc.nextLine());
+            for (int i=0; i< multimedia.length; i++) {
+                if(ele == 0) {return;}
+                if(i+1 == ele) {
+                    if(multimedia[i] != null) {
+                        if(multimedia[i] instanceof ElementoRiproducibile) {
+                            ElementoRiproducibile e = (ElementoRiproducibile) multimedia[i];
+                            e.play();
+                        } else if(multimedia[i] instanceof Immagine) {
+                            Immagine imm = (Immagine) multimedia[i];
+                            imm.show();
+                        } else {
+                            System.out.println("Elemento sconosciuto!!!");
+                        }
+                    } else {
+                        System.out.println("Empty");
+                    }
+                }
+            }
+        } while(true);
     }
 }
