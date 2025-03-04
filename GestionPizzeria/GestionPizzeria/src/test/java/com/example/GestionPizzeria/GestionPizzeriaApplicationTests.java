d02d4d9a79d7bf569152cbb9e7ee7e2a0f023d9f
emanueleumberto <info@emanueleumberto.it>
2025-03-04 14:16:00 +0100

    Traccia Esercizio 2

---
+++ GestionPizzeria/GestionPizzeria/src/test/java/com/example/GestionPizzeria/GestionPizzeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GestionPizzeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPizzeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
